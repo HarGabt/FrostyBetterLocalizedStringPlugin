45d6955c3fcc90b11865f83afeec09f9fcd95212
shoushou <shoushou1106@icloud.com>
2025-10-12 21:18:45 -0700

    Add files via upload

---
+++ Extensions/LocalizedStringEditorMenuExtension.cs
@@ -0,0 +1,22 @@
+﻿using BetterLocalizedStringPlugin.Controls;
+using Frosty.Core;
+using System.Windows.Media;
+
+namespace BetterLocalizedStringPlugin.Extensions
+{
+    public class LocalizedStringViewerMenuExtension : MenuExtension
+    {
+        internal static ImageSource imageSource = new ImageSourceConverter().ConvertFromString("pack://application:,,,/BetterLocalizedStringPlugin;component/Images/LocalizedStringEditor.png") as ImageSource;
+
+        public override string TopLevelMenuName => "View";
+
+        public override string MenuItemName => "Better Localized String Editor";
+
+        public override ImageSource Icon => imageSource;
+
+        public override RelayCommand MenuItemClicked => new RelayCommand((o) =>
+        {
+            App.EditorWindow.OpenEditor("Better Localized String Editor", new LocalizedStringEditor());
+        });
+    }
+}
